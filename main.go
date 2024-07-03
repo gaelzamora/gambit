48a16ac9775b6c87526f1fa258403a94ce120ead
gaelzamora <jgaelhz@gmail.com>
2024-07-03 13:42:03 -0600

    Last changes

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/gaelzamora/gambit/awsgo"
+	"github.com/gaelzamora/gambit/bd"
+	"github.com/gaelzamora/gambit/handlers"
+	
+	/*
+		"github.com/aws/aws-lambda-go/lambda/messages"
+		"github.com/gaelzamora/gambit/handlers"
+	*/
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayV2HTTPRequest) (*events.APIGatewayProxyResponse, error) {
+
+	awsgo.InicializoAWS()
+
+	if !ValidoParametros() {
+		panic("Error en los parametros, debe enviar 'SecretName', 'UrlPrefix'")
+	}
+
+	var res *events.APIGatewayProxyResponse
+	path := strings.Replace(request.RawPath, os.Getenv("UrlPrefix"), "", -1)
+	method := request.RequestContext.HTTP.Method
+	body := request.Body
+	header := request.Headers
+
+	bd.ReadSecret()
+
+	status, message := handlers.Manejadores(path, method, body, header, request)
+
+	headersResp := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	res = &events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body: string(message),
+		Headers: headersResp,
+	}
+
+	return res, nil
+}
+
+ 
+func ValidoParametros() bool {
+	
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	return traeParametro
+}
