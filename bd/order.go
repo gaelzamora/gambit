96fc263aaaea9d4e862f0b55f8df6821598d3dd8
gaelzamora <jgaelhz@gmail.com>
2024-07-15 09:27:21 -0600

    Laast changes

--- bd/order.go
+++ bd/order.go
@@ -35,7 +35,7 @@ func InsertOrder(o models.Orders) (int64, error) {
 	}
 
 	for _, od := range o.OrderDetails {
-		sentence = "INSERT INTO order_detail (OD_OrderId, OD_ProdId, OD_Quantity, OD_Price) VALUES (" + strconv.Itoa(int(LastInsertId))
+		sentence = "INSERT INTO orders_detail (OD_OrderId, OD_ProdId, OD_Quantity, OD_Price) VALUES (" + strconv.Itoa(int(LastInsertId))
 		sentence +=	","+strconv.Itoa(od.OD_ProdId)+","+strconv.Itoa(od.OD_Quantity)+","+strconv.FormatFloat(od.OD_Price, 'f', -1, 64)+")"
 
 		fmt.Println(sentence)
